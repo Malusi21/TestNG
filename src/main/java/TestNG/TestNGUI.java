de1a5c9fb51dc112a4b653362c664dc6f2f28331
Malusi Msomi <malusi2051@gmail.com>
2022-01-18 18:03:34 +0200

    Completing smoke test in TestNG

--- src/main/java/TestNG/TestNGUI.java
+++ src/main/java/TestNG/TestNGUI.java
@@ -4,12 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
+
+import static org.junit.Assert.*;
 
 public class TestNGUI {
     WebDriver driver = null;
@@ -22,10 +21,19 @@ public class TestNGUI {
         String chrome_path = "C:\\Users\\malusi.msomi\\Documents\\GitHub\\TestNG-attempt\\src\\main\\resources\\drivers\\chromedriver.exe";
         String gumtree_label = "/html/body/div[1]/div[3]/header/div[3]/nav/div[2]/span/span[contains(@class,\"label\")]";
         String cache_proceed_btn = "/html/body//div[2]/button[contains(.,\"Proceed\")]";
+        String username_field = "//*[contains(concat( \" \", @class, \" \" ), concat( \" \", \"input-main\", \" \" ))]";
+        String password_field = "/html/body/div[1]/section/div[2]/div/div/div[2]/div[4]/form/div[2]/input";
+        String login_button = "/html/body//form/div[4]/div[2]/button[contains(.,\"Log In\")]";
+        String valid_username = "Malusi2051@gmail.com";
+        String invalid_username = "Malusi@test.com";
+        String valid_password = "Legend!@12";
+        String invalid_password = "1234";
+        String Error_test = "/html/body/div[1]/section/div[2]/div/div/div[1]";
+        String password_error_dialog = "//*[contains(concat( \" \", @class, \" \" ), concat( \" \", \"login-form-error\", \" \" ))]";
+        String getPassword_error_text = "Please correct the errors in red below.";
 
     }
 
-    @BeforeClass
     public void SetChromeBrowserDriver(){
         System.setProperty("webdriver.chrome.driver",var.chrome_path);
         driver = new ChromeDriver();
@@ -56,8 +64,53 @@ public class TestNGUI {
         }
     }
 
-    @Test
-    public void ClickProceed(){
+    @Test (priority = 1)
+    public void Valid_Login_Test(){
+        User_Login(var.valid_username,var.valid_password);
+    }
+
+    @Test (priority = 2)
+    public void Invalid_Login_Test(){
+        User_Login(var.invalid_username,var.invalid_password);
+    }
+
+    public void User_Login(String username, String password){
+        click_proceed();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        enter_login_details(username,var.username_field);
+        enter_login_details(password, var.password_field);
+        click_login_button();
+        verify_login();
+    }
+    public void verify_login(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        boolean found = driver.findElement(By.xpath(var.login_button)).isDisplayed();
+        System.out.println(found);
+        if (found){
+            Incorrect_login();
+        }
+    }
+    public void Incorrect_login(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.xpath(var.Error_test)).isDisplayed();
+        String errorTest = driver.findElement(By.xpath(var.password_error_dialog)).getText();
+        assertEquals(errorTest, var.getPassword_error_text);
+        assertTrue(driver.findElement(By.xpath(var.login_button)).isDisplayed());
+    }
+    public void click_login_button(){
+        driver.findElement(By.xpath(var.login_button)).isDisplayed();
+        driver.findElement(By.xpath(var.login_button)).click();
+    }
+    public void enter_login_details(String string,String field){
+        driver.findElement(By.xpath(field)).isDisplayed();
+        driver.findElement(By.xpath(field)).click();
+        driver.findElement(By.xpath(field)).sendKeys(string);
+    }
+    public void click_proceed(){
         driver.findElement(By.xpath(var.cache_proceed_btn)).isDisplayed();
         driver.findElement(By.xpath(var.cache_proceed_btn)).click();
         driver.findElement(By.xpath(var.gumtree_label)).isDisplayed();
@@ -67,6 +120,9 @@ public class TestNGUI {
     @AfterMethod
     public void close_the_browser(){
         driver.close();
+    }
+    @AfterClass
+    public void quit_driver(){
         driver.quit();
     }
 }
--- src/main/java/TestNG/TestNgBasics.java
+++ src/main/java/TestNG/TestNgBasics.java
@@ -5,48 +5,48 @@ import org.testng.annotations.*;
 public class TestNgBasics {
     @BeforeClass //3
     public void Launch_browser(){
-        System.out.println("Launch browser");
+        System.out.println("Launch browser - BeforeClass");
     }
 
     @BeforeMethod //4 //7
     public void Enter_url(){
-        System.out.println("Enter the url");
+        System.out.println("Enter the url - BeforeMethod");
     }
 
     @BeforeTest //2
     public void login(){
-        System.out.println("Login");
+        System.out.println("Login - BeforeTest");
     }
 
     @BeforeSuite //1
     public void SetUp(){
-        System.out.println("setup test");
+        System.out.println("setup test - BeforeSuite");
     }
 
     @Test //5
     public void GetGoogleTitle(){
-        System.out.println("google test for the page title");
+        System.out.println("google test for the page title - Test");
     }
 
     @Test //8
     public void GetGoogleUrl(){
-        System.out.println("google test for the url");
+        System.out.println("google test for the url - Test");
     }
     @AfterMethod //6 //9
     public void Logout(){
-        System.out.println("logout");
+        System.out.println("logout - AfterMethod");
     }
 
     @AfterTest //11
     public void ClearCookies(){
-        System.out.println("clear browser cookies");
+        System.out.println("clear browser cookies - AfterTest");
     }
     @AfterClass //10
     public void closeBrowser(){
-        System.out.println("close browser");
+        System.out.println("close browser - AfterClass");
     }
     @AfterSuite //12
     public void GenerateReport(){
-        System.out.println("get report");
+        System.out.println("get report - AfterSuite");
     }
 }
